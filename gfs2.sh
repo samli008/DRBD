a0f5ce753735130ca68034e2873e53382fef696b
liyang <235938019@qq.com>
2020-04-16 09:11:15 +0000

    liyang

--- gfs2.sh
+++ gfs2.sh
@@ -13,7 +13,7 @@ pcs resource create dlm ocf:pacemaker:controld op monitor interval=30s on-fail=f
 mkfs.gfs2 -j 2 -p lock_dlm -t cluster1:gfs1 /dev/drbd0
 
 # create fs-clone
-pcs resource create fs ocf:heartbeat:Filesystem device="/dev/drbd0" directory="/data" fstype="gfs2" --clone
+pcs resource create fs ocf:heartbeat:Filesystem device="/dev/drbd0" directory="/kvm" fstype="gfs2" --clone
 
 # create HA-vm
 pcs resource create c7-1 VirtualDomain hypervisor="qemu:///system" config="/kvm/c7-1.xml" migration_transport=ssh --group group1
